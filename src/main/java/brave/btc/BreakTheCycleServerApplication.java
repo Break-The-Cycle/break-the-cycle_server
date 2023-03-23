679c312e05aaaa5a62c8286a382e263335cb9891
JamesWithCode <minseok.jeong.james@gmail.com>
2023-03-24 00:37:25 +0900

    프로젝트 생성

---
+++ src/main/java/brave/btc/BreakTheCycleServerApplication.java
@@ -0,0 +1,13 @@
+package brave.btc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakTheCycleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreakTheCycleServerApplication.class, args);
+	}
+
+}
