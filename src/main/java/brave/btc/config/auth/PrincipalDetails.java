c6dc887009050b15d061a6267d7d8260ddc3f53a
ktk3939 <rkdxorud3939@naver.com>
2023-04-16 14:44:14 +0900

    feat : 세션에 저장되는 유저정보 클래스 구현

---
+++ src/main/java/brave/btc/config/auth/PrincipalDetails.java
@@ -0,0 +1,64 @@
+package brave.btc.config.auth;
+
+import brave.btc.domain.app.user.UsePerson;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Data
+@NoArgsConstructor
+public class PrincipalDetails implements UserDetails {
+
+    private UsePerson user;
+
+    public PrincipalDetails(UsePerson user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return "ROLE_USER";
+            }
+        });
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getLoginId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
