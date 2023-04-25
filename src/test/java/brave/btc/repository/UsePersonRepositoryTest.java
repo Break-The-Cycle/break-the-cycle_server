843916e64c8a09755fd76226978faf0c2116f16b
JamesWithCode <minseok.jeong.james@gmail.com>
2023-04-25 18:08:09 +0900

    chore : gradle skip test

--- src/test/java/brave/btc/repository/UsePersonRepositoryTest.java
+++ src/test/java/brave/btc/repository/UsePersonRepositoryTest.java
@@ -9,10 +9,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import brave.btc.domain.user.UsePerson;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class UsePersonRepositoryTest {
 
     @Autowired
