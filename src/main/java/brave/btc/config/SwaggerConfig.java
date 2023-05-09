d1ef5d509a975a1f966645029760c8eb18d9c81c
ktk3939 <rkdxorud3939@naver.com>
2023-05-09 22:53:12 +0900

    feat : JWT 관련 스웨거 설정

--- src/main/java/brave/btc/config/SwaggerConfig.java
+++ src/main/java/brave/btc/config/SwaggerConfig.java
@@ -1,10 +1,13 @@
 package brave.btc.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
@@ -26,6 +29,23 @@ public class SwaggerConfig {
                 .group("BRAVE")
                 .pathsToMatch(paths)
                 .build();
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        String jwtSchemeName = "jwtAuth";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        // SecuritySchemes 등록
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP) // HTTP 방식
+                        .scheme("bearer")
+                        .bearerFormat("JWT")); // 토큰 형식을 지정하는 임의의 문자(Optional)
+
+        return new OpenAPI()
+                .addSecurityItem(securityRequirement)
+                .components(components);
     }
 
 }
