5654e3f276b381effa44382405c65d70b3cfe25c
JamesWithCode <minseok.jeong.james@gmail.com>
2023-05-21 20:29:52 +0900

    fix : cors filter 적용되는 url pattern 수정

--- src/main/java/brave/btc/config/CorsConfig.java
+++ src/main/java/brave/btc/config/CorsConfig.java
@@ -23,7 +23,7 @@ public class CorsConfig {
         config.addAllowedHeader("*");
         //모든 http method 요청 허용
         config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/api/**", config);
+        source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
 }
