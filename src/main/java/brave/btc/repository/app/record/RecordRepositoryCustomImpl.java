bdd82bd1208f3185b1c67db211f2306bba4518dd
JamesWithCode <minseok.jeong.james@gmail.com>
2023-06-06 18:46:52 +0900

    fix : 일기 날짜, 기록 조회 시 생리 기록은 제외한다.

--- src/main/java/brave/btc/repository/app/record/RecordRepositoryCustomImpl.java
+++ src/main/java/brave/btc/repository/app/record/RecordRepositoryCustomImpl.java
@@ -7,7 +7,6 @@ import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -23,15 +22,17 @@ import lombok.extern.slf4j.Slf4j;
 public class RecordRepositoryCustomImpl implements RecordRepositoryCustom{
 
 	private final JPAQueryFactory queryFactory;
-	private final Environment environment;
 	@Override
 	public List<LocalDate> searchViolentRecordDateList(int usePersonId, LocalDate fromDate, LocalDate toDate) {
+
+		List<RecordDivision> violentRecordDivisionList = Arrays.asList(RecordDivision.PICTURE, RecordDivision.DIARY, RecordDivision.RECORDING);
 		return queryFactory
 			.select(record.reportDate)
 			.from(record)
 			.where(
 				eqUsePerson(usePersonId),
-				btwReportDate(fromDate, toDate)
+				btwReportDate(fromDate, toDate),
+				record.recordDivision.in(violentRecordDivisionList)
 			)
 			.groupBy(record.reportDate)
 			.orderBy(record.reportDate.asc())
@@ -40,10 +41,13 @@ public class RecordRepositoryCustomImpl implements RecordRepositoryCustom{
 
 	@Override
 	public List<Record> searchViolentRecordList(int usePersonId, LocalDate targetDate) {
+
+		List<RecordDivision> violentRecordDivisionList = Arrays.asList(RecordDivision.PICTURE, RecordDivision.DIARY, RecordDivision.RECORDING);
 		return queryFactory.selectFrom(record)
 			.where(
 				eqUsePerson(usePersonId),
-				eqDate(targetDate)
+				eqDate(targetDate),
+				record.recordDivision.in(violentRecordDivisionList)
 			)
 			.orderBy(record.reportDate.asc())
 			.fetch();
